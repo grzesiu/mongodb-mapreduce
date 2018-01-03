31cf687ee44b2fa6168d4831be1725704c303e21
grzesiu <grzspychala@gmail.com>
2018-01-03 21:08:35 +0100

    Reorganize file

--- mongo.py
+++ mongo.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.shopping
+
+def create(db, filename):
+    with open(filename) as f:
+        for line in f:
+            db.groceries.insert({'items': line.strip().split(',')})
 
 
-with open('groceries.csv') as f:
-    for line in f:
-        db.groceries.insert({'items': line.strip().split(',')})
+def main():
+    client = MongoClient()
+    db = client.shopping
+    create(db, 'groceries.csv')
+
+
+if __name__ == '__main__':
+    main()
