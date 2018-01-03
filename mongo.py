fde22eddf6986cf6019e9e19de155a50a07a0534
grzesiu <grzspychala@gmail.com>
2018-01-03 21:52:16 +0100

    Obtain product counts

--- mongo.py
+++ mongo.py
@@ -1,3 +1,4 @@
+from bson.code import Code
 from pymongo import MongoClient
 
 
@@ -7,10 +8,33 @@ def create(db, filename):
             db.groceries.insert({'items': line.strip().split(',')})
 
 
+def get_counts(db):
+    mapper = Code("""
+    function() {
+        for(var i = 0; i < this.items.length; i++) { 
+            emit(this.items[i], 1);
+        }
+    }
+    """)
+
+    reducer = Code("""
+    function(key, values) {
+        var total = 0;
+        for(var i = 0; i < values.length; i++) {
+            total += values[i];
+        }
+        return total;
+    }
+    """)
+
+    db.groceries.map_reduce(mapper, reducer, 'counts')
+
+
 def main():
     client = MongoClient()
     db = client.shopping
-    create(db, 'groceries.csv')
+    # create(db, 'groceries.csv')
+    get_counts(db)
 
 
 if __name__ == '__main__':
